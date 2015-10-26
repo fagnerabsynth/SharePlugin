b234d011b00e9ed5cd1239a21fb177667dc9cc06
fagnerabsynth <tagnisp@hotmail.com>
2015-10-26 11:36:34 -0200

    Update ShareImplementation.cs

--- Share/Share.Forms.Plugin.Android/ShareImplementation.cs
+++ Share/Share.Forms.Plugin.Android/ShareImplementation.cs
@@ -1,4 +1,6 @@
-using Share.Forms.Plugin.Abstractions;
+
+ /* using da namespace da interface */
+
 using System;
 using Xamarin.Forms;
 using Android.OS;
@@ -7,11 +9,11 @@ using Android.Content;
 using Android.Runtime;
 using Android.App;
 
-using Share.Forms.Plugin.Droid;
+ /* using namespace do seu projeto */
 using Android.Hardware;
 
 [assembly: Dependency(typeof(ShareImplementation))]
-namespace Share.Forms.Plugin.Droid
+namespace /* alterar colocar o namespace */ Share.Forms.Plugin.Droid
 {
     public class ShareImplementation : IShare
     {
@@ -33,12 +35,10 @@ namespace Share.Forms.Plugin.Droid
 		private void ShareOnService(string status, string title = "", string link = "")
 		{
 			var intent = new Intent(global::Android.Content.Intent.ActionSend);
-			intent.PutExtra(global::Android.Content.Intent.ExtraText, String.Format("{0} - {1}",status ?? string.Empty,link ?? string.Empty));
-			intent.PutExtra(global::Android.Content.Intent.ExtraSubject, title ?? string.Empty);
+        		intent.PutExtra(global::Android.Content.Intent.ExtraText, String.Format("{0} - {1}", status ?? string.Empty, link ?? string.Empty));
+        		intent.PutExtra(global::Android.Content.Intent.ExtraSubject, title ?? string.Empty);
 			intent.SetType("text/plain");
-			intent.SetFlags(ActivityFlags.ClearTop);
-			intent.SetFlags(ActivityFlags.NewTask);
-			Android.App.Application.Context.StartActivity(intent);
+			Forms.Context.StartActivity(intent);
 		}
 	}
 }
